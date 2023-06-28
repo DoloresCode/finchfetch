dcd759ceffbfd25b805943aaedef1b52c249eba0
Dolores Crazover <doloresnetweb@gmail.com>
2023-06-27 22:27:24 -0400

    model created and migration in progress

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Microorganism, CultureMedia # import the Microorganism and CultureMedia models from models.py
+from .models import Microorganism, CultureMedia, MorphologicalClassification # import the Microorganism, CultureMedia and MorphologicalClassification models from models.py
 
 
 # Register your models here.
 admin.site.register(Microorganism) 
 admin.site.register(CultureMedia)
+admin.site.register(MorphologicalClassification)
 # these lines will add the models to the admin panel
--- main_app/models.py
+++ main_app/models.py
@@ -4,10 +4,10 @@ from django.db import models
 class Microorganism(models.Model):
     name = models.CharField(max_length=400)
     img = models.CharField(max_length=600)
-    characteristics = models.TextField(max_length=1000)
-    description = models.TextField(max_length=1000)
-    manifestation = models.TextField(max_length=1000)
-    laboratory_diagnosis = models.TextField(max_length=1000)
+    characteristics = models.TextField(max_length=3000)
+    description = models.TextField(max_length=3000)
+    manifestation = models.TextField(max_length=3000)
+    laboratory_diagnosis = models.TextField(max_length=3000)
     verified_microorganism = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
     
@@ -20,9 +20,17 @@ class Microorganism(models.Model):
 
 class CultureMedia(models.Model):
     name = models.CharField(max_length=200)
-    description = models.TextField(max_length=600)
+    description = models.TextField(max_length=1000)
     microorganism = models.ForeignKey(Microorganism, on_delete=models.CASCADE, related_name="culture_medias")
 
     def __str__(self):
         return self.name
 
+class MorphologicalClassification(models.Model):
+
+    name = models.CharField(max_length=300)
+    # this is going to create the many to many relationship and join table
+    culture_medias = models.ManyToManyField(CultureMedia)
+
+    def __str__(self):
+        return self.name
