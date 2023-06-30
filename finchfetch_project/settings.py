d30f0430312517928ffecf92162a81f2c87f1592
Dolores Crazover <doloresnetweb@gmail.com>
2023-06-30 16:18:04 -0400

    settings modified

--- finchfetch_project/settings.py
+++ finchfetch_project/settings.py
@@ -102,16 +102,23 @@ WSGI_APPLICATION = 'finchfetch_project.wsgi.application'
 # }
 
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': os.getenv('DB_ENGINE', 'django.db.backends.postgresql'),
+#         'NAME': os.getenv('DB_NAME', 'Finchfetch_Microorganisms'),
+#         'USER': os.getenv('DB_USER'),
+#         'PASSWORD': os.getenv('DB_PW'),
+#         'HOST': os.getenv('DB_HOST'),
+#         'PORT': os.getenv('DB_PORT', '5432'),
+#         'CONN_MAX_AGE': 600,
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': os.getenv('DB_ENGINE', 'django.db.backends.postgresql'),
-        'NAME': os.getenv('DB_NAME', 'Finchfetch_Microorganisms'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PW'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT', '5432'),
-        'CONN_MAX_AGE': 600,
-    }
+    'default': dj_database_url.config(
+        # Feel free to alter this value to suit your needs.
+        conn_max_age=600
+    )
 }
 
 # Password validation
