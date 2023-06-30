9f95c287136750389ca1aec98fa01cb278d99de8
Dolores Crazover <doloresnetweb@gmail.com>
2023-06-30 00:15:56 -0400

    error fixed in settings.py for deployment

--- finchfetch_project/settings.py
+++ finchfetch_project/settings.py
@@ -53,7 +53,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.midleware.common.CommonMiddleware',
+    'corsheaders.middleware.common.CommonMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
