8458241d68f1633463981bc91f086f53d527ae46
Dolores Crazover <doloresnetweb@gmail.com>
2023-06-30 20:19:52 -0400

    static files direct ory fixed v2

--- finchfetch_project/settings.py
+++ finchfetch_project/settings.py
@@ -167,4 +167,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR,'main_app/static')]
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
