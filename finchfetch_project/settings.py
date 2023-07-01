4fe3a5309f0bf24f2ce4953fde84dc9ddf522a87
Dolores Crazover <doloresnetweb@gmail.com>
2023-06-30 21:54:07 -0400

    deployment on render

--- finchfetch_project/settings.py
+++ finchfetch_project/settings.py
@@ -31,6 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 # SECURITY WARNING: don't run with debug turned on in production!
 
 DEBUG = 'RENDER' not in os.environ
+# DEBUG = True
 
 ALLOWED_HOSTS = []
 
@@ -101,18 +102,6 @@ WSGI_APPLICATION = 'finchfetch_project.wsgi.application'
 #   }
 # }
 
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': os.getenv('DB_ENGINE', 'django.db.backends.postgresql'),
-#         'NAME': os.getenv('DB_NAME', 'Finchfetch_Microorganisms'),
-#         'USER': os.getenv('DB_USER'),
-#         'PASSWORD': os.getenv('DB_PW'),
-#         'HOST': os.getenv('DB_HOST'),
-#         'PORT': os.getenv('DB_PORT', '5432'),
-#         'CONN_MAX_AGE': 600,
-#     }
-# }
 
 DATABASES = {
     'default': dj_database_url.config(
